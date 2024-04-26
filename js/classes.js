09c183c916d53ff96f9f86a2d9a0d8f04f4cf57e
Louis Thériault <116024728+TheRealDAZL@users.noreply.github.com>
2024-04-25 20:45:50 -0400

    Mise à jour affichages + nouveau bouton

--- js/classes.js
+++ js/classes.js
@@ -395,17 +395,17 @@ class Liste {
             cartes += `<div class="col-xl-4 col-lg-6 col-md-12 mb-4">
                         <div class="card h-100 p-2">
                             <div class="card-body">
-                                <p class="card-title couleurTexte fs-4">Objet no. ${this.messages[index].id + 1}</p>
+                                <p class="card-title couleurTexte font-size-pTexte">Objet no. ${this.messages[index].id + 1}</p>
                                 <img src="${this.messages[index].image}" class="img-fluid" alt="Image de l'objet">
-                                <p class="card-text"><span class="couleurTexte fs-5">Message initial :</span><br><span class="couleurObjet fs-5">${this.messages[index].message.toString()}</span><br>
-                                                     <span class="couleurTexte fs-5">Message final :</span><br><span class="couleurObjet fs-5">${this.messages[index].resultat}</span><br>
-                                                     <span class="couleurTexte fs-5">Clé de substitution :</span><br><span class="couleurObjet fs-5">${this.messages[index].cleSubstitution.toString()}</span><br>
-                                                     <span class="couleurTexte fs-5">Clé de transposition :</span><br><span class="couleurObjet fs-5">${this.messages[index].cleTransposition.toString()}</span><br>
-                                                     <span class="couleurTexte fs-5">Librairie de caractères :</span><br><span class="couleurObjet fs-5">${this.messages[index].librairie.toString()}</span><br>
-                                                     <span class="couleurTexte fs-5">Sens du chiffrement :</span><br><span class="couleurObjet fs-5">${sens}</span></p>
+                                <p class="card-text invisible"><span class="couleurTexte font-size-pCartes">Message initial :</span><br><span class="couleurObjet font-size-pCartes">${this.messages[index].message.toString()}</span><br>
+                                                     <span class="couleurTexte font-size-pCartes">Message final :</span><br><span class="couleurObjet font-size-pCartes">${this.messages[index].resultat}</span><br>
+                                                     <span class="couleurTexte font-size-pCartes">Clé de substitution :</span><br><span class="couleurObjet font-size-pCartes">${this.messages[index].cleSubstitution.toString()}</span><br>
+                                                     <span class="couleurTexte font-size-pCartes">Clé de transposition :</span><br><span class="couleurObjet font-size-pCartes">${this.messages[index].cleTransposition.toString()}</span><br>
+                                                     <span class="couleurTexte font-size-pCartes">Librairie de caractères :</span><br><span class="couleurObjet font-size-pCartes">${this.messages[index].librairie.toString()}</span><br>
+                                                     <span class="couleurTexte font-size-pCartes">Sens du chiffrement :</span><br><span class="couleurObjet font-size-pCartes">${sens}</span></p>
                             </div>
-                            <button class="btn btn-primary col-md-9 mx-auto mb-2" onclick="listeDeMessages.modifierMessage(${index})" type="button">Modifier cet objet</button>
-                            <button class="btn btn-danger col-md-9 mx-auto mb-2" onclick="listeDeMessages.supprimerMessage(${index})" type="button">Supprimer cet objet</button>
+                            <button class="btn btn-primary col-md-9 mx-auto mb-2 font-size-pCartes" onclick="listeDeMessages.modifierMessage(${index})" type="button">Modifier cet objet</button>
+                            <button class="btn btn-danger col-md-9 mx-auto mb-2 font-size-pCartes" onclick="listeDeMessages.supprimerMessage(${index})" type="button">Supprimer cet objet</button>
                         </div>
                       </div>`
         }
